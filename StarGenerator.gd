5e09f198e1124e7a468d9071abf24f399e3c2473
Tiffany Bennett <tiffany@tiffnix.com>
2023-10-03 21:02:51 -0700

    split out star code into addon

---
+++ StarGenerator.gd
@@ -0,0 +1,193 @@
+@tool
+extends "res://addons/starlight/StarManager.gd"
+
+@export var size: float = 5000: set = _set_extents
+@export var star_count: int = 10000: set = _set_star_count
+@export var rng_seed: int = 1234: set = _set_rng_seed
+
+
+var _regenerate = true
+
+
+func _set_extents(value):
+	size = value
+	_regenerate = true
+
+
+func _set_star_count(value):
+	star_count = value
+	_regenerate = true
+
+
+func _set_rng_seed(value):
+	rng_seed = value
+	_regenerate = true
+
+
+class RangeF:
+	var min: float
+	var max: float
+
+	func _init(min: float, max: float):
+		self.min = min
+		self.max = max
+
+	func sample(value: float):
+		return (max - min) * value + min
+
+
+class StarClass:
+	var weight: int
+	var stellar_class: String
+	var temp_range: RangeF
+	var luminosity_range: RangeF
+	var mass_range: RangeF
+
+	func _init(dict: Dictionary):
+		self.weight = dict.weight
+		self.stellar_class = dict.stellar_class
+		self.temp_range = dict.temp_range
+		self.luminosity_range = dict.luminosity_range
+		self.mass_range = dict.mass_range
+
+	func sample(value: float):
+		return {
+			stellar_class = stellar_class,
+			temp = temp_range.sample(value),
+			luminosity = luminosity_range.sample(value),
+			mass = mass_range.sample(value),
+		}
+
+	func get_star(position: Vector3, value: float):
+		var p = self.sample(value)
+		return Star.new(position, p.luminosity, p.temp)
+
+
+var class_O = StarClass.new({
+	weight = 1,
+	stellar_class = "O",
+	temp_range = RangeF.new(30_000, 60_000),
+	luminosity_range = RangeF.new(30_000_00, 60_000_00),
+	mass_range = RangeF.new(16_00, 32_00),
+})
+var class_B = StarClass.new({
+	weight = 13,
+	stellar_class = "B",
+	temp_range = RangeF.new(10_000, 30_000),
+	luminosity_range = RangeF.new(25_00, 30_000_00),
+	mass_range = RangeF.new(2_10, 16_00),
+})
+var class_A = StarClass.new({
+	weight = 60,
+	stellar_class = "A",
+	temp_range = RangeF.new(7500, 10_000),
+	luminosity_range = RangeF.new(5_00, 25_00),
+	mass_range = RangeF.new(1_40, 2_10),
+})
+var class_F = StarClass.new({
+	weight = 3_00,
+	stellar_class = "F",
+	temp_range = RangeF.new(6000, 7500),
+	luminosity_range = RangeF.new(1_50, 5_00),
+	mass_range = RangeF.new(1_04, 1_40),
+})
+var class_G = StarClass.new({
+	weight = 7_60,
+	stellar_class = "G",
+	temp_range = RangeF.new(5200, 6000),
+	luminosity_range = RangeF.new(60, 1_50),
+	mass_range = RangeF.new(0_80, 1_04),
+})
+var class_K = StarClass.new({
+	weight = 12_10,
+	stellar_class = "K",
+	temp_range = RangeF.new(3700, 5200),
+	luminosity_range = RangeF.new(8, 60),
+	mass_range = RangeF.new(0_45, 0_80),
+})
+var class_M = StarClass.new({
+	weight = 76_45,
+	stellar_class = "M",
+	temp_range = RangeF.new(2400, 3700),
+	luminosity_range = RangeF.new(0, 8),
+	mass_range = RangeF.new(0_08, 0_45),
+})
+
+
+var star_table = [
+	class_O,
+	class_B,
+	class_A,
+	class_F,
+	class_G,
+	class_K,
+	class_M,
+]
+
+
+func sample_sphere(rng: RandomNumberGenerator, radius: float):
+	while true:
+		var pos = Vector3(
+			rng.randf_range(-1.0, 1.0),
+			rng.randf_range(-1.0, 1.0),
+			rng.randf_range(-1.0, 1.0)
+		)
+		if pos.length_squared() <= 1.0:
+			return pos * radius
+
+
+func random_category(rng: RandomNumberGenerator):
+	var sum = 0
+	for category in star_table:
+		sum += category.weight;
+
+	var weight = rng.randi_range(1, sum - 1)
+
+	sum = 0
+	for category in star_table:
+		var prev = sum
+		sum += category.weight;
+		if weight <= sum:
+			return category
+
+
+func random_star(rng: RandomNumberGenerator):
+	var sum = 0
+	for category in star_table:
+		sum += category.weight;
+
+	var weight = rng.randi_range(1, sum - 1)
+
+	sum = 0
+	for category in star_table:
+		var prev = sum
+		sum += category.weight;
+		if weight <= sum:
+			var value = (weight - prev) / category.weight
+			var pos = sample_sphere(rng, size)
+			return category.get_star(pos, value)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if not _regenerate:
+		return
+
+	_regenerate = false
+
+	var rng = RandomNumberGenerator.new()
+	rng.seed = rng_seed
+
+	var stars = []
+
+	stars.push_back(class_G.get_star(Vector3.ZERO, 0.5))
+
+	for i in range(1, star_count):
+		var category = random_category(rng)
+		if not category:
+			continue
+
+		stars.push_back(category.get_star(sample_sphere(rng, size), rng.randf()))
+
+	set_star_list(stars)
+
---
+++ StarManager.gd
@@ -0,0 +1,123 @@
+@tool
+extends MultiMeshInstance3D
+
+class Star:
+	# Position of the star, in light years.
+	var position: Vector3
+	# Luminosity of the star, relative to the luminosity of the Sun. (L☉)
+	var luminosity: float
+	# Temperature of the star, in kelvin.
+	var temperature: float
+
+	func _init(position: Vector3, luminosity: float, temperature: float):
+		self.position = position
+		self.luminosity = luminosity
+		self.temperature = temperature
+
+
+var _rebuild_instances = true
+var _star_list: Array[Star] = []
+
+
+func set_star_list(star_list: Array[Star]):
+	_rebuild_instances = true
+	_star_list = star_list
+
+
+func blackbody_to_rgb(kelvin):
+	var temperature = kelvin / 100.0
+	var red
+	var green
+	var blue
+
+	if temperature < 66.0:
+		red = 255
+	else:
+		# a + b x + c Log[x] /.
+		# {a -> 351.97690566805693`,
+		# b -> 0.114206453784165`,
+		# c -> -40.25366309332127
+		#x -> (kelvin/100) - 55}
+		red = temperature - 55.0
+		red = 351.97690566805693 + 0.114206453784165 * red - 40.25366309332127 * log(red)
+		if red < 0:
+			red = 0
+		if red > 255:
+			red = 255
+
+	# Calculate green
+
+	if temperature < 66.0:
+		# a + b x + c Log[x] /.
+		# {a -> -155.25485562709179`,
+		# b -> -0.44596950469579133`,
+		# c -> 104.49216199393888`,
+		# x -> (kelvin/100) - 2}
+		green = temperature - 2
+		green = (
+			-155.25485562709179
+			- 0.44596950469579133 * green
+			+ 104.49216199393888 * log(green)
+		)
+		if green < 0:
+			green = 0
+		if green > 255:
+			green = 255
+
+	else:
+		# a + b x + c Log[x] /.
+		# {a -> 325.4494125711974`,
+		# b -> 0.07943456536662342`,
+		# c -> -28.0852963507957`,
+		# x -> (kelvin/100) - 50}
+		green = temperature - 50.0
+		green = 325.4494125711974 + 0.07943456536662342 * green - 28.0852963507957 * log(green)
+		if green < 0:
+			green = 0
+		if green > 255:
+			green = 255
+
+	# Calculate blue
+
+	if temperature >= 66.0:
+		blue = 255
+	else:
+		if temperature <= 20.0:
+			blue = 0
+		else:
+			# a + b x + c Log[x] /.
+			# {a -> -254.76935184120902`,
+			# b -> 0.8274096064007395`,
+			# c -> 115.67994401066147`,
+			# x -> kelvin/100 - 10}
+			blue = temperature - 10
+			blue = (
+				-254.76935184120902
+				+ 0.8274096064007395 * blue
+				+ 115.67994401066147 * log(blue)
+			)
+			if blue < 0:
+				blue = 0
+			if blue > 255:
+				blue = 255
+
+	return Color(red / 255.0, green / 255.0, blue / 255.0)
+
+
+func _process(_delta):
+	if not _rebuild_instances:
+		return
+
+	_rebuild_instances = false
+	# Throws an error when trying to set properties otherwise.
+	multimesh.instance_count = 0
+	multimesh.use_colors = true
+	multimesh.use_custom_data = true
+	multimesh.instance_count = _star_list.size()
+
+	for i in range(_star_list.size()):
+		var star = _star_list[i]
+		var transform = Transform3D().translated(star.position * max(1.0, star.luminosity / 40_000))
+		multimesh.set_instance_transform(i, transform)
+		multimesh.set_instance_color(i, blackbody_to_rgb(star.temperature))
+		multimesh.set_instance_custom_data(i, Color(star.luminosity, 0, 0))
